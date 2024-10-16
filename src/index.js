f7d688df84cae64e5e159de4dc4adcbc003f246b
olton <serhii@pimenov.com.ua>
2024-10-16 21:11:44 +0300

    add progress indicator and status messages

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import { mkdirSync, copyFileSync } from "fs";
 import { resolve, basename } from "path";
 import {globSync} from "glob";
+import chalk from "chalk";
+import ora from "ora";
 
 const defaults = {
     files: "**/*",
     skip: [],
-    dest: "."
+    dest: ".",
+    text: "Coping files...",
+    failText: "Copy failed with %s errors.",
+    succeedText: "Copy process completed in %s ms!",
+    spinner: "dots",
+    spinnerColor: "cyan"
 }
 
 export default options => {
@@ -15,6 +22,8 @@ export default options => {
         name: 'copy',
         setup(build) {
             build.onEnd(() => {
+                const startTime = Date.now()
+                const spinner = ora({ text: o.text, spinner: o.spinner, color: o.spinnerColor }).start();
                 try {
                     const files = globSync(o.files, { ignore: o.skip });
                     let dest = o.dest || '.';
@@ -27,8 +36,11 @@ export default options => {
                             copyFileSync(resolve(file), dest + filename);
                         }
                     }
+                    spinner.succeed(`${o.succeedText.replace('%s', chalk["cyanBright"](Date.now() - startTime))}`);
                 } catch (error) {
-                    console.error("Error copying files:", error);
+                    spinner.fail(`${o.failText.replace('%s', error.message)}`);
+                } finally {
+                    spinner.stop();
                 }
             });
         }
